2f806a564ef52d3cd3e3634de595c2fce8b60740
Benjamin Hugo <bennahugo@gmail.com>
2022-09-03 11:13:43 +0200

    Update Jenkinsfile.sh

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -23,7 +23,7 @@ export SINGULARITY_PULLFOLDER=$PROJECTS_DIR/.stimela_images
 mkdir $PROJECTS_DIR/.stimela_images
 
 cd $TEST_OUTPUT_DIR
-virtualenv $PROJECTS_DIR/venv -p python3
+virtualenv $PROJECTS_DIR/venv -p python3.6
 source $PROJECTS_DIR/venv/bin/activate
 pip install $PROJECTS_DIR/VermeerKAT
 pip install nose
